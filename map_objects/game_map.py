0385509852e6156aa8301de46171a93a2ed90651
Robert Gervais <rgervais@enova.com>
2020-11-29 16:54:47 -0600

    add damage/hp

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -4,6 +4,9 @@ from random import randint
 from entity import Entity
 from map_objects.tile import Tile
 from map_objects.rectangle import Rect
+from render_functions import RenderOrder
+from components.fighter import Fighter
+from components.ai import BasicMonster
 
 
 class GameMap:
@@ -101,10 +104,18 @@ class GameMap:
 
             if not any([entity for entity in entities if entity.x == x and entity.y == y]):
                 if randint(0, 100) < 80:
-                    monster = Entity(x, y, 'o', libtcod.desaturated_green, 'Orc', blocks=True)
-                else:
-                    monster = Entity(x, y, 'T', libtcod.darker_green, 'Troll', blocks=True)
+                    fighter_component = Fighter(hp=10, defense=0, power=3)
+                    ai_component = BasicMonster()
 
+                    monster = Entity(x, y, 'o', libtcod.desaturated_green, 'Orc', blocks=True,
+                                     render_order=RenderOrder.ACTOR, fighter=fighter_component, ai=ai_component)
+                else:
+                    fighter_component = Fighter(hp=16, defense=1, power=4)
+                    ai_component = BasicMonster()
+
+                    monster = Entity(x, y, 'T', libtcod.darker_green, 'Troll', blocks=True, fighter=fighter_component,
+                                     render_order=RenderOrder.ACTOR, ai=ai_component)
+                                     
                 entities.append(monster)
 
     def is_blocked(self, x, y):
