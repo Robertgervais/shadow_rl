fc5f72febf1fadc4cbe3cfcae48b19ce2bae263e
Robert Gervais <rgervais@enova.com>
2020-11-29 22:02:52 -0600

    items

--- components/fighter.py
+++ components/fighter.py
@@ -1,6 +1,7 @@
-import tcod as libtcod
+import libtcodpy as libtcod
 
 from game_messages import Message
+
 
 class Fighter:
     def __init__(self, hp, defense, power):
@@ -19,6 +20,12 @@ class Fighter:
 
         return results
 
+    def heal(self, amount):
+        self.hp += amount
+
+        if self.hp > self.max_hp:
+            self.hp = self.max_hp
+
     def attack(self, target):
         results = []
 
@@ -31,5 +38,5 @@ class Fighter:
         else:
             results.append({'message': Message('{0} attacks {1} but does no damage.'.format(
                 self.owner.name.capitalize(), target.name), libtcod.white)})
-        
+
         return results
