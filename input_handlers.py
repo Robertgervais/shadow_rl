ad7106b3f2ccd1b3910a77f697027c8d06668129
Robert Gervais <rgervais@enova.com>
2020-12-05 17:50:16 -0600

    add save function/level up/multiple dungeon levels

--- input_handlers.py
+++ input_handlers.py
@@ -12,6 +12,12 @@ def handle_keys(key, game_state):
         return handle_targeting_keys(key)
     elif game_state in (GameStates.SHOW_INVENTORY, GameStates.DROP_INVENTORY):
         return handle_inventory_keys(key)
+    elif game_state == GameStates.LEVEL_UP:
+        return handle_level_up_menu(key)
+    elif game_state == GameStates.CHARACTER_SCREEN:
+        return handle_character_screen(key)
+    elif key_char == 'z':
+        return {'wait': True}
 
     return {}
 
@@ -45,6 +51,13 @@ def handle_player_turn_keys(key):
 
     elif key_char == 'd':
         return {'drop_inventory': True}
+
+    elif key.vk == libtcod.KEY_ENTER:
+        return {'take_stairs': True}
+
+    elif key_char == 'c':
+        return {'show_character_screen': True}
+
 
     if key.vk == libtcod.KEY_ENTER and key.lalt:
         # Alt+Enter: toggle full screen
@@ -91,6 +104,38 @@ def handle_inventory_keys(key):
         return {'fullscreen': True}
     elif key.vk == libtcod.KEY_ESCAPE:
         # Exit the menu
+        return {'exit': True}
+
+    return {}
+
+def handle_main_menu(key):
+    key_char = chr(key.c)
+
+    if key_char == 'a':
+        return {'new_game': True}
+    elif key_char == 'b':
+        return {'load_game': True}
+    elif key_char == 'c' or  key.vk == libtcod.KEY_ESCAPE:
+        return {'exit': True}
+
+    return {}
+
+def handle_level_up_menu(key):
+    if key:
+        key_char = chr(key.c)
+
+        if key_char == 'a':
+            return {'level_up': 'hp'}
+        elif key_char == 'b':
+            return {'level_up': 'str'}
+        elif key_char == 'c':
+            return {'level_up': 'def'}
+
+    return {}
+
+
+def handle_character_screen(key):
+    if key.vk == libtcod.KEY_ESCAPE:
         return {'exit': True}
 
     return {}
